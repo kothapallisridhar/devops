71a48ab8f80f8ae3c4147dc7c8980a47aa167d3d
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2026-06-18 11:00:08 +0530

    conversation

---
+++ shell-script/02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Sridhar: Hello BMW!"
+echo "BMW: Thank you for contacting BMW, how can I help you!"
+echo "I need my X5 to be serviced"
+echo "Sure, will send the driver for picking up your car"
+echo "Thank you!"
