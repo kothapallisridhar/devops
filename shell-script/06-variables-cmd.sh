51c8afe31e290fb650eb79b6267d2f461103a809
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2026-06-18 11:42:35 +0530

    command line

--- shell-script/06-variables-cmd.sh
+++ shell-script/06-variables-cmd.sh
@@ -6,6 +6,6 @@ MODEL=$3
 
 echo "$OWNER: Hello $CAR!"
 echo "$CAR: Thank you for contacting $CAR, how can I help you!"
-echo "I need my $MODEL to be serviced"
-echo "Sure, will send the driver for picking up your car"
-echo "Thank you!!!"
+echo "$OWNER: I need my $MODEL to be serviced"
+echo "$CAR: Sure, will send the driver for picking up your car"
+echo "$OWNER: Thank you!!!"
