29b2b44b8ce4020c742c440bdc230c1e9b03023d
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2026-06-18 11:06:07 +0530

    variables

---
+++ shell-script/03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CUSTOMER=Sridhar
+CAR=BMW
+MODEL=X5
+
+echo "$CUSTOMER: Hello $CAR!"
+echo "BMW: Thank you for contacting $CAR, how can I help you!"
+echo "I need my $MODEL to be serviced"
+echo "Sure, will send the driver for picking up your car"
+echo "Thank you!!!"
