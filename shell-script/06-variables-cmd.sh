9734bfec4c3e7d0a221327f98ae03a198c0f2d3f
Sridhar Kothapalli <srkothapalli.in@gmail.com>
2026-06-18 11:40:11 +0530

    variables command line

---
+++ shell-script/06-variables-cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OWNER=$1
+CAR=$2
+MODEL=$3
+
+echo "$OWNER: Hello $CAR!"
+echo "$CAR: Thank you for contacting $CAR, how can I help you!"
+echo "I need my $MODEL to be serviced"
+echo "Sure, will send the driver for picking up your car"
+echo "Thank you!!!"
